f34ee7350d00efeadcb592127b6128780a905db6
Yang Dong <andyyoung01@gmail.comm>
2020-12-11 15:51:58 +0800

    added echo

--- getimage.sh
+++ getimage.sh
@@ -4,9 +4,38 @@ set -e
 
 docker login  --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD  registry.cn-shenzhen.aliyuncs.com
 
-for i in `cat imagelist.txt`
+for i in `sed -n "1,10p" imagelist.txt`
 do
 	docker pull $i
 	docker tag $i registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
 	docker push registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
 done
+
+echo "=============1-10 done==============="
+
+for i in `sed -n "11,20p" imagelist.txt`
+do
+	docker pull $i
+	docker tag $i registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
+	docker push registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
+done
+
+echo "=============11-20 done==============="
+
+for i in `sed -n "21,30p" imagelist.txt`
+do
+	docker pull $i
+	docker tag $i registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
+	docker push registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
+done
+
+echo "=============21-30 done==============="
+
+for i in `sed -n "31,41p" imagelist.txt`
+do
+	docker pull $i
+	docker tag $i registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
+	docker push registry.cn-shenzhen.aliyuncs.com/andyyoung01/${i##*/}
+done
+
+echo "done"
